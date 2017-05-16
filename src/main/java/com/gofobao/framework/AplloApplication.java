5d89f987dc3b1cee822698e472ee1103fa2ce8c8
max <595785682@qq.com>
2017-05-16 10:36:53 +0800

    项目初始化

---
+++ src/main/java/com/gofobao/framework/AplloApplication.java
@@ -0,0 +1,12 @@
+package com.gofobao.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplloApplication.class, args);
+	}
+}
