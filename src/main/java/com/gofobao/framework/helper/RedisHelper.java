d76e5365cc87a74f6298c5dff750ebb6acfe2e76
max <595785682@qq.com>
2017-05-17 19:03:25 +0800

    添加短信验证

---
+++ src/main/java/com/gofobao/framework/helper/RedisHelper.java
@@ -0,0 +1,131 @@
+package com.gofobao.framework.helper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * redis 工具类
+ * Created by Max on 17/2/15.
+ */
+@Component
+public class RedisHelper {
+    /**
+     * 失效时间默认：3600秒
+     */
+    public static final int DEFAULT_EXPIRE_TIME = 60 * 60;
+
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    public RedisHelper(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    /**
+     * 向Redis中插入String类型数据(默认失效时间：3600秒)；
+     *
+     * @param key    主键
+     * @param value  值
+     * @param expire 过期时间 默认时间：3600秒
+     * @throws Exception
+     */
+    public void put(String key, String value, Integer expire) throws Exception {
+        if ((StringUtils.isEmpty(key)) || (StringUtils.isEmpty(value))) {
+            throw new Exception("key/value 为空");
+        }
+
+        if (expire == null) {
+            expire = DEFAULT_EXPIRE_TIME;
+        }
+        try {
+            stringRedisTemplate.opsForValue().set(key, value, expire, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    /**
+     * 向Redis中插入String类型数据(永久缓存， 需要手动删除)
+     *
+     * @param key   主键
+     * @param value 值
+     * @throws Exception
+     */
+    public void put(String key, String value) throws Exception {
+        if ((StringUtils.isEmpty(key)) || (StringUtils.isEmpty(value))) {
+            throw new Exception("key/value 为空");
+        }
+
+        try {
+            stringRedisTemplate.opsForValue().set(key, value);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+
+    /**
+     * 从缓存中获取数据
+     *
+     * @param key          主键
+     * @param defaultValue 默认返回值
+     * @throws Exception
+     */
+    public String get(String key, String defaultValue) throws Exception {
+        if (StringUtils.isEmpty(key)) {
+            throw new Exception("key 为空");
+        }
+
+        try {
+            String value = stringRedisTemplate.opsForValue().get(key);
+            return value == null ? defaultValue : value.toString();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+
+    /**
+     * 根据key删除缓存中的数据
+     *
+     * @param key 主键
+     * @throws Exception
+     */
+    public void remove(String key) throws Exception {
+        if (StringUtils.isEmpty(key)) {
+            throw new Exception("key 为空");
+        }
+
+        try {
+            stringRedisTemplate.delete(key);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+
+    /**
+     * 判断key是否存在
+     *
+     * @param key 待验证key
+     * @return true：存在， false：不存在
+     * @throws Exception
+     */
+    public boolean hasKey(String key) throws Exception {
+        if (StringUtils.isEmpty(key)) {
+            throw new Exception("key 为空");
+        }
+
+        try {
+            return stringRedisTemplate.hasKey(key);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+}
--- src/main/java/com/gofobao/framework/message/entity/GfbSmsConfigEntity.java
+++ src/main/java/com/gofobao/framework/message/entity/GfbSmsConfigEntity.java
@@ -1,5 +1,9 @@
 package com.gofobao.framework.message.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
 
@@ -7,165 +11,44 @@ import java.sql.Timestamp;
  * Created by Max on 17/5/17.
  */
 @Entity
-@Table(name = "gfb_sms_config", schema = "gofobao9", catalog = "")
+@Table(name = "gfb_sms_config")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GfbSmsConfigEntity {
-    private int id;
-    private String aliasCode;
-    private String config;
-    private String configName;
-    private Integer isDel;
-    private Integer isActive;
-    private Timestamp createTime;
-    private Timestamp updateTime;
-    private Integer createId;
-    private Integer updateId;
-    private String note;
-
     @Id
     @Column(name = "ID")
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    private int id;
     @Basic
     @Column(name = "ALIAS_CODE")
-    public String getAliasCode() {
-        return aliasCode;
-    }
-
-    public void setAliasCode(String aliasCode) {
-        this.aliasCode = aliasCode;
-    }
-
+    private String aliasCode;
     @Basic
     @Column(name = "CONFIG")
-    public String getConfig() {
-        return config;
-    }
-
-    public void setConfig(String config) {
-        this.config = config;
-    }
+    private String config;
 
     @Basic
     @Column(name = "CONFIG_NAME")
-    public String getConfigName() {
-        return configName;
-    }
-
-    public void setConfigName(String configName) {
-        this.configName = configName;
-    }
-
+    private String configName;
     @Basic
     @Column(name = "IS_DEL")
-    public Integer getIsDel() {
-        return isDel;
-    }
-
-    public void setIsDel(Integer isDel) {
-        this.isDel = isDel;
-    }
-
+    private Integer isDel;
     @Basic
     @Column(name = "IS_ACTIVE")
-    public Integer getIsActive() {
-        return isActive;
-    }
-
-    public void setIsActive(Integer isActive) {
-        this.isActive = isActive;
-    }
-
+    private Integer isActive;
     @Basic
     @Column(name = "CREATE_TIME")
-    public Timestamp getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Timestamp createTime) {
-        this.createTime = createTime;
-    }
-
+    private Timestamp createTime;
     @Basic
     @Column(name = "UPDATE_TIME")
-    public Timestamp getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Timestamp updateTime) {
-        this.updateTime = updateTime;
-    }
+    private Timestamp updateTime;
 
     @Basic
     @Column(name = "CREATE_ID")
-    public Integer getCreateId() {
-        return createId;
-    }
-
-    public void setCreateId(Integer createId) {
-        this.createId = createId;
-    }
-
+    private Integer createId;
     @Basic
     @Column(name = "UPDATE_ID")
-    public Integer getUpdateId() {
-        return updateId;
-    }
-
-    public void setUpdateId(Integer updateId) {
-        this.updateId = updateId;
-    }
-
+    private Integer updateId;
     @Basic
     @Column(name = "NOTE")
-    public String getNote() {
-        return note;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GfbSmsConfigEntity that = (GfbSmsConfigEntity) o;
-
-        if (id != that.id) return false;
-        if (aliasCode != null ? !aliasCode.equals(that.aliasCode) : that.aliasCode != null) return false;
-        if (config != null ? !config.equals(that.config) : that.config != null) return false;
-        if (configName != null ? !configName.equals(that.configName) : that.configName != null) return false;
-        if (isDel != null ? !isDel.equals(that.isDel) : that.isDel != null) return false;
-        if (isActive != null ? !isActive.equals(that.isActive) : that.isActive != null) return false;
-        if (createTime != null ? !createTime.equals(that.createTime) : that.createTime != null) return false;
-        if (updateTime != null ? !updateTime.equals(that.updateTime) : that.updateTime != null) return false;
-        if (createId != null ? !createId.equals(that.createId) : that.createId != null) return false;
-        if (updateId != null ? !updateId.equals(that.updateId) : that.updateId != null) return false;
-        if (note != null ? !note.equals(that.note) : that.note != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (aliasCode != null ? aliasCode.hashCode() : 0);
-        result = 31 * result + (config != null ? config.hashCode() : 0);
-        result = 31 * result + (configName != null ? configName.hashCode() : 0);
-        result = 31 * result + (isDel != null ? isDel.hashCode() : 0);
-        result = 31 * result + (isActive != null ? isActive.hashCode() : 0);
-        result = 31 * result + (createTime != null ? createTime.hashCode() : 0);
-        result = 31 * result + (updateTime != null ? updateTime.hashCode() : 0);
-        result = 31 * result + (createId != null ? createId.hashCode() : 0);
-        result = 31 * result + (updateId != null ? updateId.hashCode() : 0);
-        result = 31 * result + (note != null ? note.hashCode() : 0);
-        return result;
-    }
+    private String note;
 }
