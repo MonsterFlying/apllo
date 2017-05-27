6adbfd1e6770df4257f783874f643892b1bf8dd8
zql <2289181978@qq.com>
2017-05-27 11:28:10 +0800

    合并代码

--- src/test/java/com/gofobao/framework/AplloApplicationTests.java
+++ src/test/java/com/gofobao/framework/AplloApplicationTests.java
@@ -8,10 +8,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Example;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
