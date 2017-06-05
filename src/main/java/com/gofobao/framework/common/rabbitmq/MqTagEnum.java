1d8b38d5f7914e8c6ad87f85b72932dcc7ab735f
max <595785682@qq.com>
2017-06-05 15:42:20 +0800

    添加mq延迟，并且实现MQ注册监听

--- src/main/java/com/gofobao/framework/common/rabbitmq/MqTagEnum.java
+++ src/main/java/com/gofobao/framework/common/rabbitmq/MqTagEnum.java
@@ -19,7 +19,8 @@ public enum MqTagEnum {
     SMS_REGISTER("SMS_REGISTER"),
     USER_ACTIVE_REGISTER("SMS_REGISTER"),  // 用户注册
     FIRST_VERIFY("FIRST_VERIFY"), //初审
-    AGAIN_VERIFY("AGAIN_VERIFY"); //复审
+    AGAIN_VERIFY("AGAIN_VERIFY"), //复审
+    NOTICE_PUBLISH("NOTICE_PUBLISH"); // 站内信通知
     private String value;
 
     private MqTagEnum(String value) {
