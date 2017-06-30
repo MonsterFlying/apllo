0b414e14dfda18b006743c9ae4d7b511d1f5b939
wangguo <wangguoflying@163.com>
2017-06-30 17:37:54 +0800

    完善红包

--- src/main/java/com/gofobao/framework/scheduler/repository/TaskSchedulerRepository.java
+++ src/main/java/com/gofobao/framework/scheduler/repository/TaskSchedulerRepository.java
@@ -1,16 +1,10 @@
 package com.gofobao.framework.scheduler.repository;
 
 import com.gofobao.framework.scheduler.entity.TaskScheduler;
-import lombok.Data;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.util.Date;
 import java.util.List;
 
 /**
