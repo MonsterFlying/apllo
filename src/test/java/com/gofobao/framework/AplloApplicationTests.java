286168a60e9f89b60363f9e1ac78bbf5a2da62b6
zql <2289181978@qq.com>
2017-07-03 15:42:53 +0800

    Merge branch 'dev' of https://git.coding.net/itissue/apllo into dev
    # Conflicts:
    #	src/main/java/com/gofobao/framework/borrow/biz/impl/BorrowBizImpl.java

--- src/test/java/com/gofobao/framework/AplloApplicationTests.java
+++ src/test/java/com/gofobao/framework/AplloApplicationTests.java
@@ -80,7 +80,7 @@ public class AplloApplicationTests {
 
         Gson gson = new Gson();
         Map<String, String> map = new HashMap<>();
-        map.put("borrowId", "169746");
+        map.put("borrowId", "169755");
         System.out.println(gson.toJson(map));
         System.out.println(SecurityHelper.getSign(gson.toJson(map)));
     }
@@ -131,6 +131,100 @@ public class AplloApplicationTests {
         System.out.println(response);
     }
 
+    @Test
+    public void test() {
 
+        //根据手机号查询存管账户
+        //findAccountByMobile();
+        //受托支付
+        //trusteePay();
+        //签约查询
+        //creditAuthQuery();
+        //取消批次
+        //batchCancel();
+        //查询投资人购买债权
+        //creditInvestQuery();
+
+        /*Map<String,String> map = new HashMap<>();
+        map.put("borrowId","169755");
+        try {
+            borrowProvider.doFirstVerify(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+        /*VoRepayReq voRepayReq = new VoRepayReq();
+        voRepayReq.setRepaymentId(168683L);
+        voRepayReq.setUserId(901L);
+        voRepayReq.setIsUserOpen(false);
+        voRepayReq.setInterestPercent(1d);
+        try {
+            repaymentBiz.repay(voRepayReq);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(true + "");*/
+
+        /*VoQueryThirdBorrowList voQueryThirdBorrowList = new VoQueryThirdBorrowList();
+        voQueryThirdBorrowList.setBorrowId(165200L);
+        voQueryThirdBorrowList.setUserId(901L);
+        voQueryThirdBorrowList.setPageNum("1");
+        voQueryThirdBorrowList.setPageSize("10");
+        DebtDetailsQueryResp resp = borrowThirdBiz.queryThirdBorrowList(voQueryThirdBorrowList);
+        System.out.println((resp.getTotalItems()));*/
+
+        Map<String,String> msg = new HashMap<>();
+        msg.put("borrowId","169755");
+        try {
+            borrowProvider.doAgainVerify(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //"userId\":901,\"repaymentId\":168675,\"interestPercent\":0.0,\"isUserOpen\":true
+        /*VoRepayReq voRepayReq = new VoRepayReq();
+        voRepayReq.setUserId(901L);
+        voRepayReq.setRepaymentId(168675L);
+        voRepayReq.setInterestPercent(0.0);
+        voRepayReq.setIsUserOpen(false);
+        try {
+            repaymentBiz.repay(voRepayReq);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+
+        /*BidApplyQueryReq request = new BidApplyQueryReq();
+        request.setAccountId("6212462040000600025");
+        request.setChannel(ChannelContant.HTML);
+        request.setOrgOrderId("GFBT_1498530231199");
+        BidApplyQueryResp response = jixinManager.send(JixinTxCodeEnum.BID_APPLY_QUERY, request, BidApplyQueryResp.class);
+        System.out.println(response);*/
+
+        /*BatchDetailsQueryReq request = new BatchDetailsQueryReq();
+        request.setBatchNo("115714");
+        request.setBatchTxDate("20170630");
+        request.setType("9");
+        request.setPageNum("1");
+        request.setPageSize("10");
+        request.setChannel(ChannelContant.HTML);
+        BatchDetailsQueryResp response = jixinManager.send(JixinTxCodeEnum.BATCH_DETAILS_QUERY, request, BatchDetailsQueryResp.class);
+        System.out.println(response);*/
+
+        /*Borrow borrow = borrowService.findById(165225L);
+        try {
+            borrowBiz.notTransferedBorrowAgainVerify(borrow);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+        /*Borrow borrow = borrowService.findById(165227L);
+        try {
+            borrowBiz.transferedBorrowAgainVerify(borrow);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    }
 
 }
