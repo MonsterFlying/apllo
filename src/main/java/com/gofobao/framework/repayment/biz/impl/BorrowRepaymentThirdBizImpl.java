8d69f311e206c5bc520ed8f2ca579c1c426ac704
max <595785682@qq.com>
2017-08-08 16:44:05 +0800

    更新垫付流程

--- src/main/java/com/gofobao/framework/repayment/biz/impl/BorrowRepaymentThirdBizImpl.java
+++ src/main/java/com/gofobao/framework/repayment/biz/impl/BorrowRepaymentThirdBizImpl.java
@@ -436,9 +436,10 @@ public class BorrowRepaymentThirdBizImpl implements BorrowRepaymentThirdBiz {
      * @param borrow
      * @param order
      * @param advanceAssetChanges
-     * @return
+     * @param lateDays
+     *@param lateInterest @return
      */
-    public List<BailRepay> calculateAdvancePlan(Borrow borrow, int order,  List<AdvanceAssetChange> advanceAssetChanges) throws Exception {
+    public List<BailRepay> calculateAdvancePlan(Borrow borrow, int order, List<AdvanceAssetChange> advanceAssetChanges, int lateDays, long lateInterest) throws Exception {
         /* 代偿记录集合 */
         List<BailRepay> bailRepayList = new ArrayList<>();
         /* 查询投资列表 */
@@ -463,12 +464,10 @@ public class BorrowRepaymentThirdBizImpl implements BorrowRepaymentThirdBiz {
         Preconditions.checkNotNull("投资回款记录不存在!");
         Map<Long/* tenderId */, BorrowCollection> borrowCollectionMaps = borrowCollectionList.stream().collect(Collectors.toMap(BorrowCollection::getTenderId, Function.identity()));
 
-        long txFeeIn = 0;//投资方手续费  利息管理费
         long txAmount = 0;/* 垫付金额 = 垫付本金 + 垫付利息 */
         long intAmount = 0;/* 交易利息 */
         long principal = 0;/* 还款本金 */
         for (Tender tender : tenderList) {
-            txFeeIn = 0;
             //垫付资金变动
             AdvanceAssetChange advanceAssetChange = new AdvanceAssetChange();
             advanceAssetChanges.add(advanceAssetChange);
@@ -495,25 +494,21 @@ public class BorrowRepaymentThirdBizImpl implements BorrowRepaymentThirdBiz {
             advanceAssetChange.setInterest(intAmount);
             advanceAssetChange.setPrincipal(principal);
 
-            //利息管理费
-            if (((borrow.getType() == 0) || (borrow.getType() == 4))) {
-                Set<String> stockholder = new HashSet<>(Arrays.asList("2480", "1753", "1699", "3966", "1413", "1857", "183", "2327", "2432", "2470", "2552", "2739", "3939", "893", "608", "1216"));
-                if (!stockholder.contains(tender.getUserId())) {
-                    /* 利息管理费 */
-                    long interestFee = (long) MathHelper.myRound(intAmount * 0.1, 2);
-                    advanceAssetChange.setInterestFee(interestFee);
-                    txFeeIn += interestFee;
-                }
+
+            if ((lateDays > 0) && (lateInterest > 0)) {  //借款人逾期罚息
+                int overdueFee = new Double(tender.getValidMoney() / new Double(borrow.getMoney()) * lateInterest / 2).intValue();// 出借人收取50% 逾期管理费 ;
+                advanceAssetChange.setOverdueFee(overdueFee);
+                intAmount += overdueFee ;
             }
+
             /* 垫付金额 */
             txAmount = principal + intAmount; //= 垫付本金 + 垫付利息
-
             String orderId = JixinHelper.getOrderId(JixinHelper.REPAY_BAIL_PREFIX);
             BailRepay bailRepay = new BailRepay();
             bailRepay.setOrderId(orderId);
             bailRepay.setTxAmount(StringHelper.formatDouble(txAmount, 100, false));
             bailRepay.setTxCapAmout(StringHelper.formatDouble(principal, 100, false));
-            bailRepay.setTxIntAmount(StringHelper.formatDouble(intAmount - txFeeIn, 100, false));
+            bailRepay.setTxIntAmount(StringHelper.formatDouble(intAmount, 100, false));
             bailRepay.setOrgOrderId(StringHelper.toString(tender.getThirdTenderOrderId()));
             bailRepay.setOrgTxAmount(StringHelper.formatDouble(tender.getValidMoney(), 100, false));
             bailRepay.setForAccountId(tenderUserThirdAccount.getAccountId());
