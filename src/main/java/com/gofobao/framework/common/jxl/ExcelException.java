a442e5b5628a29cc3248f649f48dc3c15b50ccaf
max <595785682@qq.com>
2017-08-09 15:41:44 +0800

    添加修改红包

--- src/main/java/com/gofobao/framework/common/jxl/ExcelException.java
+++ src/main/java/com/gofobao/framework/common/jxl/ExcelException.java
@@ -5,22 +5,17 @@ package com.gofobao.framework.common.jxl;
  */
 public class ExcelException extends Exception {
 
-    public ExcelException() {
-        // TODO Auto-generated constructor stub
-    }
+    public ExcelException() {}
 
     public ExcelException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
     }
 
     public ExcelException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
     }
 
     public ExcelException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
     }
 }
