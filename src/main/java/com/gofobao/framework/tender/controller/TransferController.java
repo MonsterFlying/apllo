5f4b3b231ffc79fcbacc28b7a684d26928b989f3
zql <2289181978@qq.com>
2017-08-09 14:15:19 +0800

    修改还款bug

--- src/main/java/com/gofobao/framework/tender/controller/TransferController.java
+++ src/main/java/com/gofobao/framework/tender/controller/TransferController.java
@@ -167,7 +167,6 @@ public class TransferController {
     }
 
     @ApiOperation("购买债券记录")
-    @Autowired
     @GetMapping("v2/transfer/user/list/{borrowId}")
     private ResponseEntity<VoBorrowTenderUserWarpListRes> tenderList(@PathVariable Long borrowId) {
         return transferBiz.transferUserList(borrowId);
