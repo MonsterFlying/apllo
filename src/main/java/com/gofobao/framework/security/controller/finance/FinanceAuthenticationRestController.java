3b8c866442773ff9f7564a4373ad6b6cf5c04cac
SimpleReader <474536890@qq.com>
2017-08-15 19:55:48 +0800

    还款

--- src/main/java/com/gofobao/framework/security/controller/finance/FinanceAuthenticationRestController.java
+++ src/main/java/com/gofobao/framework/security/controller/finance/FinanceAuthenticationRestController.java
@@ -29,7 +29,7 @@ public class FinanceAuthenticationRestController {
 
     @PostMapping("/login")
     public ResponseEntity<VoBasicUserInfoResp> login(HttpServletRequest httpServletRequest, HttpServletResponse response, @ModelAttribute VoLoginReq voLoginReq){
-        return userBiz.login(httpServletRequest, response, voLoginReq) ;
+        return userBiz.financeLogin(httpServletRequest, response, voLoginReq) ;
     }
 
 
