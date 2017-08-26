dfcb28524d8e64c6ffba96f01b35f565f7b4e8dd
SimpleReader <wangguoflying@163.com>
2017-08-26 12:02:28 +0800

    修改已知bug

--- src/main/java/com/gofobao/framework/message/entity/SmsNoticeSettingsEntity.java
+++ src/main/java/com/gofobao/framework/message/entity/SmsNoticeSettingsEntity.java
@@ -19,7 +19,6 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class SmsNoticeSettingsEntity implements Serializable{
     @Id
-    @GeneratedValue
     @Column(name = "user_id")
     private Long userId ;
     @Basic
