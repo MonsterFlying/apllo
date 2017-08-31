19ff2e0e2054ec54971cea488d56f0c5fe6ad26a
max <595785682@qq.com>
2017-08-31 18:33:53 +0800

    提交用户资产同步

--- src/main/java/com/gofobao/framework/asset/controller/AssetController.java
+++ src/main/java/com/gofobao/framework/asset/controller/AssetController.java
@@ -3,6 +3,7 @@ package com.gofobao.framework.asset.controller;
 import com.gofobao.framework.asset.biz.AssetBiz;
 import com.gofobao.framework.asset.vo.request.VoSynAssetsRep;
 import com.gofobao.framework.asset.vo.response.*;
+import com.gofobao.framework.member.vo.response.VoBasicUserInfoResp;
 import com.gofobao.framework.security.contants.SecurityContants;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -27,12 +28,18 @@ public class AssetController {
     }
 
 
-    @ApiOperation("资金同步问题")
+    @ApiOperation("投标中心资金同步问题")
     @PostMapping("/asset/v2/synOffLineRecharge")
     public ResponseEntity<VoUserAssetInfoResp> synOffLineRecharge(@ApiIgnore @RequestAttribute(SecurityContants.USERID_KEY) Long userId) throws Exception {
         return assetBiz.synOffLineRecharge(userId) ;
     }
 
+
+    @ApiOperation("资产中心资金同步问题")
+    @PostMapping("/home/v2/synHome")
+    public ResponseEntity<VoAssetIndexResp> synHome(@ApiIgnore @RequestAttribute(SecurityContants.USERID_KEY) Long userId) throws Exception {
+        return assetBiz.synHome(userId) ;
+    }
 
     @ApiOperation("资金同步问题")
     @PostMapping("/pub/asset/v2/adminSynOffLineRecharge")
