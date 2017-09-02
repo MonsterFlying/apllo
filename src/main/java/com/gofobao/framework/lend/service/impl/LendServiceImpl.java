2cc4006e7072444a16e73470ef0aaf16db93ba6a
SimpleReader <wangguoflying@163.com>
2017-09-02 17:13:53 +0800

    Merge branch 'dev' of https://git.coding.net/itissue/apllo into dev
    # Conflicts:
    #	src/main/java/com/gofobao/framework/scheduler/BorrowRepayScanduler.java

--- src/main/java/com/gofobao/framework/lend/service/impl/LendServiceImpl.java
+++ src/main/java/com/gofobao/framework/lend/service/impl/LendServiceImpl.java
@@ -144,7 +144,7 @@ public class LendServiceImpl implements LendService {
                 lend.setSpend(1d);
                 lend.setStatusStr(LendContants.STATUS_YES_STR);
             }
-            lend.setAvatar(StringUtils.isEmpty(user.getAvatarPath())?javaDomain+"static/images/user/default_avatar.jpg":user.getAvatarPath());
+            lend.setAvatar(StringUtils.isEmpty(user.getAvatarPath())?javaDomain+"/static/images/user/default_avatar.jpg":user.getAvatarPath());
             lend.setReleaseAt(DateHelper.dateToString(p.getCreatedAt()));
             lend.setCollectionAt(DateHelper.dateToString(p.getRepayAt()));
             lend.setSpend(Double.parseDouble(StringHelper.formatMon(p.getMoneyYes() / new Double(p.getMoney()))));
