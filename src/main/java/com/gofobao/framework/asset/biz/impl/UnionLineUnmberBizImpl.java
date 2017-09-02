4f9cf936914825fd8ce2391d520c801001160df6
SimpleReader <wangguoflying@163.com>
2017-09-02 14:18:38 +0800

    修改已知bug

--- src/main/java/com/gofobao/framework/asset/biz/impl/UnionLineUnmberBizImpl.java
+++ src/main/java/com/gofobao/framework/asset/biz/impl/UnionLineUnmberBizImpl.java
@@ -8,6 +8,8 @@ import com.gofobao.framework.asset.vo.request.VoUnionLineNoReq;
 import com.gofobao.framework.asset.vo.response.pc.UnionLineNo;
 import com.gofobao.framework.asset.vo.response.pc.UnionLineNoWarpRes;
 import com.gofobao.framework.core.vo.VoBaseResp;
+import com.gofobao.framework.member.biz.UserBiz;
+import com.gofobao.framework.member.vo.response.VoBasicUserInfoResp;
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -20,6 +22,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by admin on 2017/8/21.
@@ -29,15 +32,22 @@ public class UnionLineUnmberBizImpl implements UnionLineNumberBiz {
 
     @Autowired
     private UnionLineNumberService unionLineNumberService;
-
+    @Autowired
+    private UserBiz userBiz;
 
     @Override
     public ResponseEntity<UnionLineNoWarpRes> list(VoUnionLineNoReq unionLineNoReq) {
         UnionLineNoWarpRes warpRes = VoBaseResp.ok("查询成功", UnionLineNoWarpRes.class);
+        ResponseEntity<VoBasicUserInfoResp> responseEntity = userBiz.userInfo(unionLineNoReq.getUserId());
+        VoBasicUserInfoResp userInfoResp = responseEntity.getBody();
+        if (Objects.isNull(userInfoResp) || StringUtils.isEmpty(userInfoResp.getSubbranch())) {
+            return ResponseEntity.ok(warpRes);
+        }
         Specification<UnionLineNumber> specification = Specifications.<UnionLineNumber>and()
                 .eq(!StringUtils.isEmpty(unionLineNoReq.getCityId()), "city", unionLineNoReq.getCityId())
                 .eq(!StringUtils.isEmpty(unionLineNoReq.getProvinceId()), "province", unionLineNoReq.getProvinceId())
-                .like(!StringUtils.isEmpty(unionLineNoReq.getKeyword()), "bankName", "%"+unionLineNoReq.getKeyword()+"%")
+                .like("bankname", "%" + userInfoResp.getSubbranch() + "%")
+                .like(!StringUtils.isEmpty(unionLineNoReq.getKeyword()), "address", "%" + unionLineNoReq.getKeyword() + "%")
                 .build();
         Page<UnionLineNumber> unionLineNumbers = unionLineNumberService.findAll(specification, new PageRequest(unionLineNoReq.getPageIndex(), unionLineNoReq.getPageSize(), new Sort(Sort.Direction.DESC, "id")));
         List<UnionLineNumber> lineNumbers = unionLineNumbers.getContent();
