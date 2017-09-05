dfe4efa0d64648404299963b1fa144b7bc2f3f75
max <595785682@qq.com>
2017-09-05 11:51:59 +0800

    添加数据提交页面

--- src/main/java/com/gofobao/framework/message/biz/impl/InitDBBizImpl.java
+++ src/main/java/com/gofobao/framework/message/biz/impl/InitDBBizImpl.java
@@ -172,7 +172,6 @@ public class InitDBBizImpl implements InitDBBiz {
                     }
                 }
             }
-            borrowRepaymentService.save(borrowRepaymentDataCache);
         }
     }
 
