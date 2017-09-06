6e238e1c5c04dbcf7f54c58bd2acacbc49116869
zql <2289181978@qq.com>
2017-09-06 14:56:33 +0800

    Merge branch 'dev' of https://git.coding.net/itissue/apllo into dev
    # Conflicts:
    #	src/test/java/com/gofobao/framework/AplloApplicationTests.java

--- src/main/java/com/gofobao/framework/system/controller/SystemController.java
+++ src/main/java/com/gofobao/framework/system/controller/SystemController.java
@@ -7,6 +7,8 @@ import com.gofobao.framework.migrate.MigrateProtocolBiz;
 import com.gofobao.framework.migrate.MigrateTenderBiz;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +33,8 @@ public class SystemController {
 
     @Autowired
     MigrateProtocolBiz migrateProtocolBiz;
+
+
 
     @GetMapping("pub/initDB/{password}/{startIndex}")
     public void initDB(@PathVariable() String password,
