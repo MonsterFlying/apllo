6f7a7cd7f2785b89ebb4378bec2ab5bab48155a1
max <595785682@qq.com>
2017-09-06 17:39:11 +0800

    修改接口访问问题

--- src/main/java/com/gofobao/framework/config/SecurityAdapter.java
+++ src/main/java/com/gofobao/framework/config/SecurityAdapter.java
@@ -12,7 +12,7 @@ public class SecurityAdapter extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new Jwtintercepter())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/**/pub/**")
+                .excludePathPatterns("/pub/**")
                 .excludePathPatterns("/swagger-resources/**", "/v2/**")
                 .excludePathPatterns("/*.html", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg")
                 .excludePathPatterns("/index/**")
