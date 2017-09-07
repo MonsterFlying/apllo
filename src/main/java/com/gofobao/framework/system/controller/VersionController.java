af6a604821069ceb59e01fcacbc18e7a1b27b6df
max <595785682@qq.com>
2017-09-07 15:21:21 +0800

    修改接口访问问题

--- src/main/java/com/gofobao/framework/system/controller/VersionController.java
+++ src/main/java/com/gofobao/framework/system/controller/VersionController.java
@@ -1,5 +1,6 @@
 package com.gofobao.framework.system.controller;
 
+import com.gofobao.framework.common.qiniu.util.StringUtils;
 import com.gofobao.framework.system.biz.SysVersionBiz;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -23,10 +24,9 @@ public class VersionController {
 
     @PostMapping("/version/checkVersion")
     @ApiOperation("版本检查 params: 版本号 versionId:1 ,请求来源  requestSource: 1:Android,2:ios,3:H5")
-    public void checkVersion(@RequestHeader("requestResource") Integer terminal,
-                             @RequestHeader("version") Integer versionId,
+    public void checkVersion(@RequestHeader("version") Integer versionId,
                              HttpServletResponse response) {
-         sysVersionBiz.list(terminal, versionId,response);
+         sysVersionBiz.list(1, versionId,response);
     }
 
 
