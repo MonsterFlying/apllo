88580926c140d27db185e1bcc44ad9f68eca3609
SimpleReader <wangguoflying@163.com>
2017-09-07 21:40:00 +0800

    版本

--- src/main/java/com/gofobao/framework/system/controller/VersionController.java
+++ src/main/java/com/gofobao/framework/system/controller/VersionController.java
@@ -24,9 +24,8 @@ public class VersionController {
     @PostMapping("/version/checkVersion")
     @ApiOperation("版本检查 params: 版本号 versionId:1 ,请求来源  requestSource: 1:Android,2:ios,3:H5")
     public void checkVersion(@RequestHeader("version") Integer versionId,
-                             @RequestHeader("requestSource") Integer requestSource,
                              HttpServletResponse response) {
-         sysVersionBiz.list(requestSource, versionId,response);
+         sysVersionBiz.list(1, versionId,response);
     }
 
 
