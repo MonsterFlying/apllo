d808a674ad6eec3d2f3c14f591b67e59674765c6
zql <2289181978@qq.com>
2017-09-07 17:43:58 +0800

    新增撤回即信红包接口

--- src/main/java/com/gofobao/framework/repayment/biz/impl/BorrowRepaymentThirdBizImpl.java
+++ src/main/java/com/gofobao/framework/repayment/biz/impl/BorrowRepaymentThirdBizImpl.java
@@ -152,12 +152,13 @@ public class BorrowRepaymentThirdBizImpl implements BorrowRepaymentThirdBiz {
         request.setProductId(borrow.getProductId());
         request.setChannel(ChannelContant.HTML);
         TrusteePayQueryResp trusteePayQueryResp = jixinManager.send(JixinTxCodeEnum.TRUSTEE_PAY_QUERY, request, TrusteePayQueryResp.class);
-        if ((ObjectUtils.isEmpty(trusteePayQueryResp)) || (!ObjectUtils.isEmpty(trusteePayQueryResp.getRetCode()) && !JixinResultContants.SUCCESS.equals(trusteePayQueryResp.getRetCode()))) {
+        if (ObjectUtils.isEmpty(trusteePayQueryResp)) {
             throw new Exception("批次放款调用：受托支付查询失败,msg->" + trusteePayQueryResp.getRetMsg());
         }
         /*收款人id*/
         long takeUserId = borrow.getTakeUserId();
-        if (!ObjectUtils.isEmpty(takeUserId) && "1".equals(trusteePayQueryResp.getState())) {
+        if (!ObjectUtils.isEmpty(takeUserId)
+                && "1".equals(trusteePayQueryResp.getState())) {
             takeUserThirdAccount = userThirdAccountService.findByUserId(takeUserId);
         }
 
