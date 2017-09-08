ab6803e85ac56e740e892081973a871746cd7391
zql <2289181978@qq.com>
2017-09-08 23:46:31 +0800

    '提交还款修改'

--- src/main/java/com/gofobao/framework/repayment/biz/Impl/RepaymentBizImpl.java
+++ src/main/java/com/gofobao/framework/repayment/biz/Impl/RepaymentBizImpl.java
@@ -1493,7 +1493,7 @@ public class RepaymentBizImpl implements RepaymentBiz {
             freezeAssetChange.setForUserId(repayUserThirdAccount.getUserId());
             freezeAssetChange.setUserId(repayUserThirdAccount.getUserId());
             freezeAssetChange.setType(AssetChangeTypeEnum.freeze);
-            freezeAssetChange.setRemark(String.format("成功还款标的[%s]%冻结资金s元", borrow.getName(), StringHelper.formatDouble(money / 100D, true)));
+            freezeAssetChange.setRemark(String.format("成功还款标的[%s]冻结资金%s元", borrow.getName(), StringHelper.formatDouble(money / 100D, true)));
             freezeAssetChange.setSeqNo(assetChangeProvider.getSeqNo());
             freezeAssetChange.setMoney(money);
             freezeAssetChange.setGroupSeqNo(assetChangeProvider.getGroupSeqNo());
