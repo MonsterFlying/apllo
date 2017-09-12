a2b7c5a92603662130319a440280557479e49102
zql <2289181978@qq.com>
2017-09-12 10:35:38 +0800

    修改自动投标规则排序

--- src/main/java/com/gofobao/framework/tender/entity/AutoTender.java
+++ src/main/java/com/gofobao/framework/tender/entity/AutoTender.java
@@ -2,6 +2,7 @@ package com.gofobao.framework.tender.entity;
 
 import lombok.Data;
 import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -13,6 +14,7 @@ import java.util.Date;
 @DynamicInsert
 @Table(name = "gfb_auto_tender")
 @Data
+@DynamicUpdate
 public class AutoTender {
     @Id
     @GeneratedValue
