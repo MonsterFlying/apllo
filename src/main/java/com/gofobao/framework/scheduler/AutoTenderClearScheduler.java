6801e3705a038b21d865925613390cf837aee840
SimpleReader <wangguoflying@163.com>
2017-09-19 19:15:33 +0800

    调度异常失败通知邮件

--- src/main/java/com/gofobao/framework/scheduler/AutoTenderClearScheduler.java
+++ src/main/java/com/gofobao/framework/scheduler/AutoTenderClearScheduler.java
@@ -4,6 +4,7 @@ import com.github.wenhao.jpa.Specifications;
 import com.gofobao.framework.common.data.DataObject;
 import com.gofobao.framework.common.data.LtSpecification;
 import com.gofobao.framework.helper.DateHelper;
+import com.gofobao.framework.helper.ExceptionEmailHelper;
 import com.gofobao.framework.tender.entity.AutoTender;
 import com.gofobao.framework.tender.service.AutoTenderService;
 import lombok.extern.slf4j.Slf4j;
@@ -29,25 +30,31 @@ public class AutoTenderClearScheduler {
     @Autowired
     private AutoTenderService autoTenderService;
 
+    @Autowired
+    private ExceptionEmailHelper exceptionEmailHelper;
+
     @Scheduled(cron = "0 4 0 * * ? ")
     @Transactional(rollbackOn = Exception.class)
     public void process() {
-        log.info("自动清除过期自动投标规则启动");
-        do {
-            Specification<AutoTender> ats = Specifications
-                    .<AutoTender>and()
-                    .predicate(new LtSpecification("autoAt", new DataObject(DateHelper.subDays(new Date(), EXPIRE_DAY))))
-                    .build();
-            List<AutoTender> autoTenderList = autoTenderService.findList(ats);
-            if (CollectionUtils.isEmpty(autoTenderList)) {
-                break;
-            }
+        try {
+            log.info("自动清除过期自动投标规则启动");
+            do {
+                Specification<AutoTender> ats = Specifications
+                        .<AutoTender>and()
+                        .predicate(new LtSpecification("autoAt", new DataObject(DateHelper.subDays(new Date(), EXPIRE_DAY))))
+                        .build();
+                List<AutoTender> autoTenderList = autoTenderService.findList(ats);
+                if (CollectionUtils.isEmpty(autoTenderList)) {
+                    break;
+                }
+                //删除过期自动投标
+                autoTenderService.delete(autoTenderList);
 
-            //删除过期自动投标
-            autoTenderService.delete(autoTenderList);
-
-            //更新自动投标序号
-            autoTenderService.updateAutoTenderOrder();
-        } while (false);
+                //更新自动投标序号
+                autoTenderService.updateAutoTenderOrder();
+            } while (false);
+        }catch (Exception e){
+            exceptionEmailHelper.sendException("调度-自动清除过期自动失败",e);
+        }
     }
 }
