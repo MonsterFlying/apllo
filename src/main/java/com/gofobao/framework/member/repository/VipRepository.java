d7576d25a4a7379b1febc8dc18e0815fbfe7c5f1
SimpleReader <wangguoflying@163.com>
2017-09-19 11:58:28 +0800

    修改已过期vip

--- src/main/java/com/gofobao/framework/member/repository/VipRepository.java
+++ src/main/java/com/gofobao/framework/member/repository/VipRepository.java
@@ -4,10 +4,12 @@ import com.gofobao.framework.member.entity.Vip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 /**
  * Created by Administrator on 2017/6/16 0016.
  */
 @Repository
 public interface VipRepository extends JpaRepository<Vip, Long> {
-    Vip findTopByUserIdAndStatus(Long userId, int status);
+    Vip findTopByUserIdAndStatusIsAndExpireAtGreaterThan(Long userId, int status, Date date);
 }
