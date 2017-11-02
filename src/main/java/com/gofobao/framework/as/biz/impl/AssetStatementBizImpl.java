c5fcd39f41974503651d88ef86540c7e2498e8f9
yuweijie@outlook.com <595785682@qq.com>
2017-11-02 17:34:36 +0800

    资金插入

--- src/main/java/com/gofobao/framework/as/biz/impl/AssetStatementBizImpl.java
+++ src/main/java/com/gofobao/framework/as/biz/impl/AssetStatementBizImpl.java
@@ -38,7 +38,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
