52483c5ad9eb7b970e650d4d92f8c9323f26846c
SimpleReader <wangguoflying@163.com>
2017-11-14 09:35:11 +0800

    理财列表排序

--- src/main/java/com/gofobao/framework/comment/service/impl/TopicReplyServiceImpl.java
+++ src/main/java/com/gofobao/framework/comment/service/impl/TopicReplyServiceImpl.java
@@ -1,6 +1,9 @@
 package com.gofobao.framework.comment.service.impl;
 
 import com.gofobao.framework.comment.service.TopicReplyService;
+import com.gofobao.framework.comment.vo.request.VoTopicReplyReq;
+import com.gofobao.framework.core.vo.VoBaseResp;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,4 +11,8 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TopicReplyServiceImpl implements TopicReplyService {
+    @Override
+    public ResponseEntity<VoBaseResp> publishReply(VoTopicReplyReq voTopicReplyReq, Long userId) {
+        return null;
+    }
 }
